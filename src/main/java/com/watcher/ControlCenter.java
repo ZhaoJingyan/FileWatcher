1c6caf4d8e00c2a8b69d4b2914bc791dddd389ab
Zhao Jinyan <949474952@qq.com>
2017-01-04 21:57:51 +0800

    修改bug，queue初始化

--- src/main/java/com/watcher/ControlCenter.java
+++ src/main/java/com/watcher/ControlCenter.java
@@ -3,11 +3,12 @@ package com.watcher;
 import java.util.concurrent.LinkedBlockingDeque;
 
 /**
- * Created by Administrator on 2017-1-1.
+ * 控制中心.
+ * Created by Zhao Jinyan on 2017-1-1.
  */
 class ControlCenter {
 
-    private static LinkedBlockingDeque<String> queue;
+    private static LinkedBlockingDeque<String> queue = new LinkedBlockingDeque<>();
 
     synchronized static void putMessage(String message){
         try {
