1a0000fdb0ecb0a6f37471b9cb21bc668785c069
Zhao Jinyan <949474952@qq.com>
2017-01-08 22:44:56 +0800

    打印模拟的传输信息

--- test.bat
+++ test.bat
@@ -1,5 +1,11 @@
 @echo off
-echo WScript.sleep 5000 > sleep.vbs
+echo WScript.sleep 1000 > sleep.vbs
 Wscript sleep.vbs
+echo 1% test info
+Wscript sleep.vbs
+echo 50% test info
+Wscript sleep.vbs
+echo 100% test info
 del sleep.vbs
-echo over
+echo test over
+exit /b 0
