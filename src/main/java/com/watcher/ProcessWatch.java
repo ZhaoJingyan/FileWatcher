667eb60c86216dc1f04eb527419536659c11447f
Zhao Jinyan <949474952@qq.com>
2017-01-08 21:14:36 +0800

    改变堵塞方式，使线程可重复调用

--- src/main/java/com/watcher/ProcessWatch.java
+++ src/main/java/com/watcher/ProcessWatch.java
@@ -1,43 +1,91 @@
 package com.watcher;
 
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Created by Administrator on 2017-1-1.
+ * 刷新bat执行时的控制台输出流.
+ * Created by Zhao Jinyan on 2017-1-1.
  */
 class ProcessWatch extends Thread {
 
-    private Process process;
+    private Process process = null;
+
+    private boolean running = true;
 
     private boolean over;
 
-    ProcessWatch(Process process){
+    private Lock lock;
+
+    private Condition condition;
+
+    ProcessWatch(){
         super();
-        this.process = process;
         over = false;
+        lock = new ReentrantLock();
+        condition = lock.newCondition();
+    }
+
+    boolean isRunning(){
+        return running;
     }
 
     @Override
     public void run() {
-        if(process == null)
-            return;
-        Scanner scanner = new Scanner(process.getInputStream());
-        while(true){
-            if(over)
-                break;
-            String before = null;
-            while(scanner.hasNext()){
-                String temp = scanner.nextLine();
-                if(before == null || !temp.equals(before)){
-                    System.out.println(temp);
-                    before = temp;
-                }
+        try{
+            while (isRunning()){
+                refresh();
             }
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void setOver(boolean over) {
+    private void refresh() throws InterruptedException{
+        lock.lockInterruptibly();
+        try{
+            if(process == null)
+                condition.await();
+            Scanner scanner = new Scanner(process.getInputStream());
+            while(true){
+                if(over)
+                    break;
+                String before = null;
+                while(scanner.hasNext()){
+                    String temp = scanner.nextLine();
+                    if(before == null || !temp.equals(before)){
+                        System.out.println(temp);
+                        before = temp;
+                    }
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void close(){
+        running = false;
+        interrupt();
+    }
+
+    void setOver(boolean over) {
         this.over = over;
+        this.process = null;
+    }
+
+    void setProcess(Process process) throws InterruptedException {
+        lock.lockInterruptibly();
+        try {
+            this.process = process;
+            this.over = false;
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
     }
 }
