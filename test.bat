d826813f632a3305a8239b9edc401f4b5f646c7a
Zhao Jinyan <949474952@qq.com>
2017-01-10 23:26:36 +0800

    显示正常的%(echa命令转义字符%)

--- test.bat
+++ test.bat
@@ -1,11 +1,11 @@
 @echo off
 echo WScript.sleep 1000 > sleep.vbs
 Wscript sleep.vbs
-echo 1% test info
+echo 1%% test info
 Wscript sleep.vbs
-echo 50% test info
+echo 50%% test info
 Wscript sleep.vbs
-echo 100% test info
+echo 100%% test info
 del sleep.vbs
 echo test over
 exit /b 0
