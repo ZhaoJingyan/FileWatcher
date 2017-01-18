33a8c7c4a67113d6f52a9d75f274d251966d7495
Zhao Jinyan <949474952@qq.com>
2017-01-18 22:48:44 +0800

    + ControlCenterAdapter

---
+++ src/main/java/com/watcher/ControlCenterAdapter.java
@@ -0,0 +1,12 @@
+package com.watcher;
+
+import java.util.List;
+
+/**
+ * Created by Zhao Jinyan on 2017/1/17.
+ */
+public interface ControlCenterAdapter {
+
+    void setTableColumn(List<FileInformationColumn> columns);
+
+}
