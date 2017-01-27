d367de0710132c02fef471fd5fc3fe085bba0bad
Zhao Jinyan <949474952@qq.com>
2017-01-27 16:57:55 +0800

    修改了构造方法超出的异常

--- src/main/java/com/watcher/FilesTable.java
+++ src/main/java/com/watcher/FilesTable.java
@@ -1,13 +1,12 @@
 package com.watcher;
 
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Files Table(文件列表).
- *
+ * <p>
  * Created by Zhao Jinyan on 2016/12/28.
  */
 class FilesTable {
@@ -18,16 +17,20 @@ class FilesTable {
 
     /**
      * 构造方法。
-     * @throws SQLException 数据库访问异常
+     *
      * @throws WatcherException 初始化失败
-     * @throws ParseException 字符串格式化转换失败
      */
-    FilesTable() throws SQLException, WatcherException, ParseException {
+    FilesTable() throws WatcherException {
         columns = new ArrayList<>();
-        List<FileInformation> informations = Resources.getFileInformations();
-        if (informations == null || informations.size() == 0)
+        List<FileInformation> allInformation;
+        try {
+            allInformation = Resources.getFileInformations();
+        } catch (SQLException e) {
+            throw new WatcherException("Fail to Read File Information From Database!", e);
+        }
+        if (allInformation == null || allInformation.size() == 0)
             throw new WatcherException("没有文件信息...");
-        for (FileInformation information : informations) {
+        for (FileInformation information : allInformation) {
             columns.add(new FileInformationColumn(information));
         }
     }
@@ -35,6 +38,7 @@ class FilesTable {
     /**
      * 当被监控的文件目录发生变化时，就是调用此方法。
      * 此方法会过滤无关文件变化消息（message），根据有效的文件变化消息更新文件列表。
+     *
      * @param message 文件变化消息，其中包含文件名，和事件类型
      * @return 如果返回为false，说明message被过滤
      */
@@ -49,7 +53,7 @@ class FilesTable {
                     System.out.printf("%s 开始监控...\n", message.data());
                     result = true;
                 }
-                if(column.getType().equals("ENTRY_CREATE") || column.getType().equals("ENTRY_MODIFY")){
+                if (column.getType().equals("ENTRY_CREATE") || column.getType().equals("ENTRY_MODIFY")) {
                     column.setType(message.type());
                     column.setTime(message.time());
                     System.out.printf("%s 刷新信息\n", message.data());
@@ -72,17 +76,18 @@ class FilesTable {
 
     /**
      * 刷新文件列表.
-     * @param time 比对时间（就是当前时间）
+     *
+     * @param time    比对时间（就是当前时间）
      * @param adapter 文件处理适配器，用于回调文件处理逻辑
      * @throws InterruptedException 堵塞中断异常
      */
     void refresh(long time, FileDisposeAdapter adapter) throws InterruptedException {
-        if(adapter == null)
+        if (adapter == null)
             return;
         synchronized (lock) {
-            for (FileInformationColumn column : columns){
-                if("ENTRY_CREATE".equals(column.getType()) || "ENTRY_MODIFY".equals(column.getType())){
-                    if(time - column.getTime() >= Resources.WATCHED_TIME()){
+            for (FileInformationColumn column : columns) {
+                if ("ENTRY_CREATE".equals(column.getType()) || "ENTRY_MODIFY".equals(column.getType())) {
+                    if (time - column.getTime() >= Resources.WATCHED_TIME()) {
                         System.out.printf("%s 10分钟无变化，开始处理...\n", column.getName());
                         column.setType("DISPOSE");
                         adapter.dispose(column.getName());
@@ -94,14 +99,15 @@ class FilesTable {
 
     /**
      * 更新文件信息.
+     *
      * @param name File Name
      */
-    void updateFile(String name){
-        if(name == null)
+    void updateFile(String name) {
+        if (name == null)
             return;
-        synchronized (lock){
-            for(FileInformationColumn column : columns){
-                if(name.equals(column.getName())){
+        synchronized (lock) {
+            for (FileInformationColumn column : columns) {
+                if (name.equals(column.getName())) {
                     column.addOneDay();
                     return;
                 }
