f9374a8cf9419f27a6588c8c006758692ce95c17
Zhao Jinyan <949474952@qq.com>
2017-01-27 17:05:05 +0800

    完善ControlCenter，其他线程发出的消息

--- src/main/java/com/watcher/ControlCenter.java
+++ src/main/java/com/watcher/ControlCenter.java
@@ -6,18 +6,24 @@ import java.util.concurrent.LinkedBlockingDeque;
  * 控制中心.
  * Created by Zhao Jinyan on 2017-1-1.
  */
-class ControlCenter {
+class ControlCenter extends ThreadAdapter {
 
-    private static LinkedBlockingDeque<String> queue = new LinkedBlockingDeque<>();
+    private static final String NAME = "Control Center";
+
+    private static LinkedBlockingDeque<Message> queue = new LinkedBlockingDeque<>();
+
+    private static ControlCenter center = null;
 
     private static FileWatcher fileWatcher = null;
 
+    // Default File Watcher
     private static class DefaultFileWatcher extends FileWatcher {
 
-        DefaultFileWatcher() {
+        DefaultFileWatcher() throws WatcherException {
             super();
         }
 
+        // Start All Threads.
         @Override
         void start() {
             // Startup threads.
@@ -29,13 +35,16 @@ class ControlCenter {
 
     }
 
-    static FileWatcher getFileWatcher() {
+    static FileWatcher getFileWatcher() throws WatcherException {
+        if(center == null){
+            center = new ControlCenter();
+        }
         if (fileWatcher == null)
             fileWatcher = new DefaultFileWatcher();
         return fileWatcher;
     }
 
-    synchronized static void putMessage(String message) {
+    static void putMessage(Message<?> message) {
         try {
             queue.put(message);
         } catch (InterruptedException e) {
@@ -43,14 +52,31 @@ class ControlCenter {
         }
     }
 
-    static String takeMessage() {
-        try {
-            return queue.take();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return null;
-        }
+    private int runThreadNum = 0;
+
+    private ControlCenter() {
+        super(NAME);
+        start();
     }
 
+    @Override
+    protected void execute() throws InterruptedException {
+        Message<?> message = queue.take();
+        if(message instanceof ThreadAdapter.ThreadMessage){
+            ThreadAdapter.ThreadMessage threadMessage = (ThreadAdapter.ThreadMessage) message;
+            System.out.printf("[%s]:%s\n", ThreadAdapter.STARTED, threadMessage.data());
+            if(ThreadAdapter.STARTED.equals(threadMessage.type()) && !NAME.equals(threadMessage.data())){
+                runThreadNum++;
+            } else if(ThreadAdapter.CLOSED.equals(threadMessage.type())){
+                runThreadNum--;
+                if(runThreadNum <= 0){
+                    System.out.println("The Program is closed.");
+                    close();
+                }
+            }
 
+        } else {
+            System.out.println("Error Message!");
+        }
+    }
 }
